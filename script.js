0eb856ca853574466451887df2355e790f7be2a0
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2020-11-19 20:51:48 -0600

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const container = document.getElementById("container");
+            json.sort((a,b) => (a.hoursInSpace < b.hoursInSpace ? 1 : -1));
+            let addedContent = "";
+            let totalAstronauts = `
+                <h2>Total Astronauts: ${json.length}</h2>
+            `;
+            for (astronaut of json){
+                addedContent += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}">
+                </div>
+                `;
+            }
+            container.innerHTML = totalAstronauts + addedContent;
+        });  
+    });
+});
